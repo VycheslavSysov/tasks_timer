d97fea054c7532632dee9f6232d1b0234997647a
VycheslavSysov <sva342007@gmail.com>
2026-03-28 21:07:34 +0200

    fix TaskDetailsPage.jsx

--- src/pages/TaskDetailsPage.jsx
+++ src/pages/TaskDetailsPage.jsx
@@ -1,21 +1,13 @@
 import {useParams, useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-  Cell
-} from "recharts";
 import formatTime from "@/utils/formatTime.js";
 
 export default function TaskDetailsPage() {
   const {id} = useParams();
   const navigate = useNavigate();
-  const task = useSelector(state => state.tasks.find(t => t.id === Number(id)));
-  const tasks = useSelector(state => state.tasks);
+  const selectedTasks = useSelector(state => state.tasks);
+  const tasks = Array.isArray(selectedTasks) ? selectedTasks : [];
+  const task = tasks.find(t => t.id === Number(id));
 
   if (!task) {
     return <div>Задачу не знайдено</div>;
@@ -32,21 +24,6 @@ export default function TaskDetailsPage() {
         <p>Початок: {new Date(task.startTime).toLocaleTimeString()}</p>
         <p>Кінець: {new Date(task.endTime).toLocaleTimeString()}</p>
         <p>Тривалість: {formatTime(task.duration)}</p>
-        <ResponsiveContainer width="100%" height={300}>
-          <BarChart data={tasks}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="duration">
-              {tasks.map((t) => (
-                  <Cell
-                      key={t.id}
-                      fill={t.id === task.id ? "#f97316" : "#2a78e8"}
-                  />
-              ))}
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
       </div>
   );
 }
